92dfce53a3b4700ac421323001cdcb2dd7deb39f
Joao Pedro Igeski Morais <joaopedroigeski@gmail.com>
2023-06-06 11:32:47 -0300

    Camada de servico, registro de componentes

---
+++ src/main/java/com/joaopedroigeskimorais/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.joaopedroigeskimorais.course.services;
+
+import com.joaopedroigeskimorais.course.entities.User;
+import com.joaopedroigeskimorais.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
